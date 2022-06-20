c3bdf38fed712ad66059bda4579b7fd77877a1dd
theArctesian <stephen.d.okita@gmail.com>
2022-06-19 20:55:41 -0700

    i hate my life

---
+++ app/src/compontents/searchbar.svelte
@@ -0,0 +1,5 @@
+<form method="get" action="https://duckduckgo.com/">
+	<input size="40" name="q" />
+
+	<input type="submit" value="Search" />
+</form>
---
+++ app/src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
+<slot />
+
+<style>
+	slot {
+		background-color: #272727;
+		color: white;
+	}
+</style>
