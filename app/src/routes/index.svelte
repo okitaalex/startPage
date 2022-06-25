8e895170234f96f9c3ffe90f8953d96ec5289c23
TheArctesian <stephen.d.okita@gmail.com>
2022-06-24 19:25:54 -0700

    some stuff

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,129 +1,129 @@
 <script>
-import Searchbar from "../compontents/searchbar.svelte"
-
+	import Searchbar from '../compontents/searchbar.svelte';
 </script>
-<Searchbar></Searchbar>
-<div class="obj">
-	<h1 class="text-center">School</h1>
-	<div class="list">
-		<ul>
-			<li>
-				<a href="https://mail.google.com/mail/u/0/#inbox" target="_blank">
-					<p>Gmail</p>
-					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-						><title>Gmail</title><path
-							d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"
-						/></svg
-					>
-				</a>
-			</li>
-			<li>
-				<a href="https://drive.google.com/drive/my-drive" target="_blank">
-					<p>Drive</p>
-					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-						><title>Google Drive</title><path
-							d="M12.01 1.485c-2.082 0-3.754.02-3.743.047.01.02 1.708 3.001 3.774 6.62l3.76 6.574h3.76c2.081 0 3.753-.02 3.742-.047-.005-.02-1.708-3.001-3.775-6.62l-3.76-6.574zm-4.76 1.73a789.828 789.861 0 0 0-3.63 6.319L0 15.868l1.89 3.298 1.885 3.297 3.62-6.335 3.618-6.33-1.88-3.287C8.1 4.704 7.255 3.22 7.25 3.214zm2.259 12.653-.203.348c-.114.198-.96 1.672-1.88 3.287a423.93 423.948 0 0 1-1.698 2.97c-.01.026 3.24.042 7.222.042h7.244l1.796-3.157c.992-1.734 1.85-3.23 1.906-3.323l.104-.167h-7.249z"
-						/></svg
-					>
-				</a>
-			</li>
-			<li>
-				<a href="https://cishk.schoology.com/home#/?_k=4c5x0x" target="_blank">
-					<p>Schoology</p>
-					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-						><title>Google Scholar</title><path
-							d="M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977 0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z"
-						/></svg
-					>
-				</a>
-			</li>
-			<li>
-				<a href="https://cishk.powerschool.com/" target="_blank">
-					<p>Powerschool</p>
-					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-						><title>PowerShell</title><path
-							d="M23.181 2.974c.568 0 .923.463.792 1.035l-3.659 15.982c-.13.572-.697 1.035-1.265 1.035H.819c-.568 0-.923-.463-.792-1.035L3.686 4.009c.13-.572.697-1.035 1.265-1.035zm-8.375 9.346c.251-.394.227-.905-.09-1.243L9.122 5.125c-.38-.404-1.037-.407-1.466-.003-.429.402-.468 1.056-.088 1.46l4.662 4.96v.11l-7.42 5.374c-.45.327-.533.977-.187 1.453.346.476.991.597 1.44.27l8.229-5.91c.28-.196.438-.365.514-.52zm-2.796 4.399a.928.928 0 00-.934.923c0 .51.418.923.934.923h4.433a.928.928 0 00.934-.923.928.928 0 00-.934-.923z"
-						/></svg
-					>
-				</a>
-			</li>
-		</ul>
-	</div>
-</div>
-<div class="obj">
-	<h1 class="text-center">School</h1>
-	<div class="list">
-		<ul>
-			<li>
-				<a href="https://mail.google.com/mail/u/0/#inbox" target="_blank">
-					<p>Gmail</p>
-					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-						><title>Gmail</title><path
-							d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"
-						/></svg
-					>
-				</a>
-			</li>
-			<li>
-				<a href="https://drive.google.com/drive/my-drive" target="_blank">
-					<p>Drive</p>
-					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-						><title>Google Drive</title><path
-							d="M12.01 1.485c-2.082 0-3.754.02-3.743.047.01.02 1.708 3.001 3.774 6.62l3.76 6.574h3.76c2.081 0 3.753-.02 3.742-.047-.005-.02-1.708-3.001-3.775-6.62l-3.76-6.574zm-4.76 1.73a789.828 789.861 0 0 0-3.63 6.319L0 15.868l1.89 3.298 1.885 3.297 3.62-6.335 3.618-6.33-1.88-3.287C8.1 4.704 7.255 3.22 7.25 3.214zm2.259 12.653-.203.348c-.114.198-.96 1.672-1.88 3.287a423.93 423.948 0 0 1-1.698 2.97c-.01.026 3.24.042 7.222.042h7.244l1.796-3.157c.992-1.734 1.85-3.23 1.906-3.323l.104-.167h-7.249z"
-						/></svg
-					>
-				</a>
-			</li>
-			<li>
-				<a href="https://cishk.schoology.com/home#/?_k=4c5x0x" target="_blank">
-					<p>Schoology</p>
-					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-						><title>Google Scholar</title><path
-							d="M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977 0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z"
-						/></svg
-					>
-				</a>
-			</li>
-			<li>
-				<a href="https://cishk.powerschool.com/" target="_blank">
-					<p>Powerschool</p>
-					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
-						><title>PowerShell</title><path
-							d="M23.181 2.974c.568 0 .923.463.792 1.035l-3.659 15.982c-.13.572-.697 1.035-1.265 1.035H.819c-.568 0-.923-.463-.792-1.035L3.686 4.009c.13-.572.697-1.035 1.265-1.035zm-8.375 9.346c.251-.394.227-.905-.09-1.243L9.122 5.125c-.38-.404-1.037-.407-1.466-.003-.429.402-.468 1.056-.088 1.46l4.662 4.96v.11l-7.42 5.374c-.45.327-.533.977-.187 1.453.346.476.991.597 1.44.27l8.229-5.91c.28-.196.438-.365.514-.52zm-2.796 4.399a.928.928 0 00-.934.923c0 .51.418.923.934.923h4.433a.928.928 0 00.934-.923.928.928 0 00-.934-.923z"
-						/></svg
-					>
-				</a>
-			</li>
-		</ul>
-	</div>
-</div>
 
-<style>
-	*{
-		background: #272727;
-		color: white;
-	}
-	h1 {
-		flex-direction: column;
-		font-size: 2rem;
-	}
-	svg {
-		width: 2rem;
-		fill: white;
-		margin-left: 1rem;
-	}
-	li {
-	}
-	a {
-		font-size: 1rem;
-		display: flex;
-	}
-	.list {
-		background-color: #272727;
-	}
-	.obj {
-		width: fit-content;
-		flex-direction: column;
-		flex-wrap: wrap;
-	}
-</style>
+<Searchbar />
+<div class="obj">
+	<h1 class="text-center">School</h1>
+	<div class="list">
+		<ul>
+			<li>
+				<a href="https://mail.google.com/mail/u/0/#inbox" target="_blank">
+					<p>Gmail</p>
+					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<title>Gmail</title>
+						<path
+							d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12
+							16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178
+							3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z" />
+					</svg>
+				</a>
+			</li>
+			<li>
+				<a href="https://drive.google.com/drive/my-drive" target="_blank">
+					<p>Drive</p>
+					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<title>Google Drive</title>
+						<path
+							d="M12.01 1.485c-2.082 0-3.754.02-3.743.047.01.02 1.708 3.001 3.774 6.62l3.76
+							6.574h3.76c2.081 0 3.753-.02
+							3.742-.047-.005-.02-1.708-3.001-3.775-6.62l-3.76-6.574zm-4.76 1.73a789.828 789.861 0 0
+							0-3.63 6.319L0 15.868l1.89 3.298 1.885 3.297 3.62-6.335 3.618-6.33-1.88-3.287C8.1
+							4.704 7.255 3.22 7.25 3.214zm2.259 12.653-.203.348c-.114.198-.96 1.672-1.88
+							3.287a423.93 423.948 0 0 1-1.698 2.97c-.01.026 3.24.042
+							7.222.042h7.244l1.796-3.157c.992-1.734 1.85-3.23 1.906-3.323l.104-.167h-7.249z" />
+					</svg>
+				</a>
+			</li>
+			<li>
+				<a href="https://cishk.schoology.com/home#/?_k=4c5x0x" target="_blank">
+					<p>Schoology</p>
+					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<title>Google Scholar</title>
+						<path
+							d="M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977
+							0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z" />
+					</svg>
+				</a>
+			</li>
+			<li>
+				<a href="https://cishk.powerschool.com/" target="_blank">
+					<p>Powerschool</p>
+					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<title>PowerShell</title>
+						<path
+							d="M23.181 2.974c.568 0 .923.463.792 1.035l-3.659 15.982c-.13.572-.697 1.035-1.265
+							1.035H.819c-.568 0-.923-.463-.792-1.035L3.686 4.009c.13-.572.697-1.035
+							1.265-1.035zm-8.375 9.346c.251-.394.227-.905-.09-1.243L9.122
+							5.125c-.38-.404-1.037-.407-1.466-.003-.429.402-.468 1.056-.088 1.46l4.662
+							4.96v.11l-7.42 5.374c-.45.327-.533.977-.187 1.453.346.476.991.597
+							1.44.27l8.229-5.91c.28-.196.438-.365.514-.52zm-2.796 4.399a.928.928 0 00-.934.923c0
+							.51.418.923.934.923h4.433a.928.928 0 00.934-.923.928.928 0 00-.934-.923z" />
+					</svg>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
+<div class="obj">
+	<h1 class="text-center">School</h1>
+	<div class="list">
+		<ul>
+			<li>
+				<a href="https://mail.google.com/mail/u/0/#inbox" target="_blank">
+					<p>Gmail</p>
+					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<title>Gmail</title>
+						<path
+							d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12
+							16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178
+							3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z" />
+					</svg>
+				</a>
+			</li>
+			<li>
+				<a href="https://drive.google.com/drive/my-drive" target="_blank">
+					<p>Drive</p>
+					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<title>Google Drive</title>
+						<path
+							d="M12.01 1.485c-2.082 0-3.754.02-3.743.047.01.02 1.708 3.001 3.774 6.62l3.76
+							6.574h3.76c2.081 0 3.753-.02
+							3.742-.047-.005-.02-1.708-3.001-3.775-6.62l-3.76-6.574zm-4.76 1.73a789.828 789.861 0 0
+							0-3.63 6.319L0 15.868l1.89 3.298 1.885 3.297 3.62-6.335 3.618-6.33-1.88-3.287C8.1
+							4.704 7.255 3.22 7.25 3.214zm2.259 12.653-.203.348c-.114.198-.96 1.672-1.88
+							3.287a423.93 423.948 0 0 1-1.698 2.97c-.01.026 3.24.042
+							7.222.042h7.244l1.796-3.157c.992-1.734 1.85-3.23 1.906-3.323l.104-.167h-7.249z" />
+					</svg>
+				</a>
+			</li>
+			<li>
+				<a href="https://cishk.schoology.com/home#/?_k=4c5x0x" target="_blank">
+					<p>Schoology</p>
+					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<title>Google Scholar</title>
+						<path
+							d="M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977
+							0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z" />
+					</svg>
+				</a>
+			</li>
+			<li>
+				<a href="https://cishk.powerschool.com/" target="_blank">
+					<p>Powerschool</p>
+					<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<title>PowerShell</title>
+						<path
+							d="M23.181 2.974c.568 0 .923.463.792 1.035l-3.659 15.982c-.13.572-.697 1.035-1.265
+							1.035H.819c-.568 0-.923-.463-.792-1.035L3.686 4.009c.13-.572.697-1.035
+							1.265-1.035zm-8.375 9.346c.251-.394.227-.905-.09-1.243L9.122
+							5.125c-.38-.404-1.037-.407-1.466-.003-.429.402-.468 1.056-.088 1.46l4.662
+							4.96v.11l-7.42 5.374c-.45.327-.533.977-.187 1.453.346.476.991.597
+							1.44.27l8.229-5.91c.28-.196.438-.365.514-.52zm-2.796 4.399a.928.928 0 00-.934.923c0
+							.51.418.923.934.923h4.433a.928.928 0 00.934-.923.928.928 0 00-.934-.923z" />
+					</svg>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
